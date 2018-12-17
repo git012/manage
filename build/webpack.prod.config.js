8895129a18e2026e578119d36c7ef0a4f0a1137b
changhaochen <932451842@qq.com>
2018-12-17 14:19:19 +0800

    chc

---
+++ build/webpack.prod.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const merge = require('webpack-merge');
+const webpackBaseConfig = require('./webpack.base.config.js');
+const fs = require('fs');
+
+fs.open('./src/config/env.js', 'w', function (err, fd) {
+    const buf = 'export default "production";';
+    fs.write(fd, buf, 0, buf.length, 0, function (err, written, buffer) {});
+});
+
+module.exports = merge(webpackBaseConfig, {
+    output: {
+        // 生产
+//      publicPath: 'http://manage.yugoubao.net/iview/',
+        // 测试
+        publicPath: 'http://tmanage.yugoubao.net/iview/',
+        filename: '[name].js?v=[hash]',
+        chunkFilename: '[name].chunk.js?v=[hash]'
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: '[name].css?v=[hash]',
+            allChunks: true
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendors',
+            filename: 'vendors.js?v=[hash]'
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new HtmlWebpackPlugin({
+            filename: '../index.html',
+            template: './src/template/index.ejs',
+            inject: false
+        })
+    ]
+});
+
